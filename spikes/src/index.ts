1837f719d814e892ca4b52a2d43a0c11560bb26f
“lragji” <22937426+LRagji@users.noreply.github.com>
2024-10-07 21:19:35 +0530

    Completed k-way merge

--- spikes/src/index.ts
+++ spikes/src/index.ts
@@ -2,3 +2,11 @@ import { TConfig } from "./t-config.js";
 import { ChunkLinker } from "./chunk-linker.js";
 import { ChunkId } from "./chunk.js";
 import { ChunkContainer } from "./chunk-container.js";
+
+
+// Invoke an Instance of chunk-container
+// Invoke an Instance of chunk-linker
+// Accept REST calls
+// Accept WebSocket calls
+// For Write collect all chunks and set data parallel.
+// For Read generate a query plan and get data parallel.
